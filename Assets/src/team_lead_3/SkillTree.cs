02963a00fef7dd48a73729a400a3106f10f9ceae
GreatJoey <ej95050@gmail.com>
2025-10-08 23:59:36 -0700

    Added the skill tree

---
+++ Assets/src/team_lead_3/SkillTree.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SkillTree : MonoBehaviour
+{
+    private string MainScene = "SkillTreeScene"; // probably will need to change this
+    public static bool is_open = false;
+    public GameObject SkillTreeMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Pressing the key");
+            if (is_open)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void LoadScene()
+    {
+        Debug.Log("Loading the main scene");
+        SceneManager.LoadScene(MainScene);
+    }
+
+    void Resume()
+    {
+        SkillTreeMenu.SetActive(false);
+        Time.timeScale = 1f;
+        is_open = false;
+    }
+    void Pause()
+    {
+        SkillTreeMenu.SetActive(true);
+        Time.timeScale = 0f;
+        is_open = true;
+    }
+}
