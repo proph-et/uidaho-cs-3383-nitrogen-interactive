57faba5bdd32b709891abf7200f792f6b378cb18
TorryChmelik <torry2.chmelik2@gmail.com>
2025-10-29 19:38:32 -0700

    TL6(Fixed Pause Bug, Updated MainMenu, Updated Pause Menu)

--- Assets/src/team_lead_6/Scripts/MainMenu.cs
+++ Assets/src/team_lead_6/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1); 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player Has Quit the Game");
     }
 }
