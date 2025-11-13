af0b1b006d853203c1bd10fc326a40fa5f7cbd39
prophet <andersenfwestphal@gmail.com>
2025-11-13 01:56:12 -0800

    integrated shop ui and sound

---
+++ Assets/src/team_lead_1/scripts/ShopInterfaceManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopInterfaceManager : MonoBehaviour
+{
+    [SerializeField] private string Scene = "ShopInterfaceScene";
+    private bool sceneOn = false;
+
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(Scene, LoadSceneMode.Additive);
+        sceneOn = true;
+    }
+}
