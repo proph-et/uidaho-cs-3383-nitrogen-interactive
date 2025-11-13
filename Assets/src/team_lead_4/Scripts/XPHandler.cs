bde5bd8cb6148171926b7ead62bd5e40f1b6cdc3
Hadix05 <meye4747@vandals.uidaho.edu>
2025-11-12 22:05:44 -0800

    idk what I added

--- Assets/src/team_lead_4/Scripts/XPHandler.cs
+++ Assets/src/team_lead_4/Scripts/XPHandler.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 public class XPHandler : Collectable
 {
-    [SerializeField] private XPSO xpData;
+    // [SerializeField] private XPSO xpData;
 
-    public override void Collect(GameObject collector)
-    {
-        base.Collect(collector);
+    // public override void Collect(GameObject collector)
+    // {
+    //     base.Collect(collector);
 
-        PlayerLevel playerLevel = collector.GetComponent<PlayerLevel>();
+    //      PlayerLevel playerLevel = collector.GetComponent<PlayerLevel>();
 
-        if (playerLevel != null)
-        {
-            playerLevel.levelSystem.AddXp(xpData.xpAmount);
-            Debug.Log($"Gained {xpData.xpAmount} XP!");
-        }
-        else
-        {
-            Debug.LogWarning("No PlayerLevel component found on collector!");
-        }
-    }
+    //     if (playerLevel != null)
+    //     {
+    //         playerLevel.levelSystem.AddXp(xpData.xpAmount);
+    //         Debug.Log($"Gained {xpData.xpAmount} XP!");
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("No PlayerLevel component found on collector!");
+    //     }
+    // }
 }
