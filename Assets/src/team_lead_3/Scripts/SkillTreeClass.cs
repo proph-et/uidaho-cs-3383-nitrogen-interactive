353132cc29bcc526278b538fdc5a3bd5760b30da
GreatJoey <ej95050@gmail.com>
2025-11-13 16:48:38 -0800

    Can now buy nodes

--- Assets/src/team_lead_3/Scripts/SkillTreeClass.cs
+++ Assets/src/team_lead_3/Scripts/SkillTreeClass.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class SkillTreeClass : Ability
 {
+
+    // protected static Health health;
     public int overallLevel = 1; 
     private LevelSystem levelSystem;
     int levReq; // the level required to unlock that skill
 
     public void SetLevelSystem(LevelSystem levelSystem)
     {
-        this.levelSystem = levelSystem;
+        //this.levelSystem = levelSystem;
+        levelSystem = LevelSystem.Instance;
     }
 
     public void Init() // no idea if this works until we call the class later
@@ -16,19 +19,20 @@ public class SkillTreeClass : Ability
 
         overallLevel = (int)LevelSystem.Instance.GetLevelNum();
         Debug.Log($"The level is {overallLevel}");
-        levelSystem.skillPoint = 1;
-        Debug.Log($"You have {levelSystem.skillPoint} skill points");
+        LevelSystem.Instance.skillPoint = 1;
+        Debug.Log($"You have {LevelSystem.Instance.skillPoint} skill points");
+        // health = new Health();
         // skillPoints = (int)levelSystem.GetLevelNum(); // just added this, this seems like a logic error
     }
     public bool AddSkill(int skillID, int levReq, int classLevel, bool flag) 
     {
         Debug.Log("ADD SKILL IS BEING CALLED");
         // Debug.Log($"YOU HAVE {skillPoints} skill points");
-        if (classLevel >= levReq && flag == true && levelSystem.skillPoint >= 1)
+        if (flag == true && LevelSystem.Instance.skillPoint >= 1)
         {
             //add the skill
             overallLevel = overallLevel + 1;
-            levelSystem.skillPoint = levelSystem.skillPoint - 1;
+            LevelSystem.Instance.skillPoint = LevelSystem.Instance.skillPoint - 1;
             return true;
         }
         else
