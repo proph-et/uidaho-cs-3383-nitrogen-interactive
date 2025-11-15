dc8ceb901fc976878c609c013f2eaab935556c17
TorryChmelik <torry2.chmelik2@gmail.com>
2025-11-14 17:55:34 -0800

    broken torry scene andersen working

--- Assets/src/team_lead_6/Scripts/HealthBar.cs
+++ Assets/src/team_lead_6/Scripts/HealthBar.cs
@@ -7,15 +7,41 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
+    public Gradient gradient;
+    //private Image fill;
+    private Health health;
+    private GameObject player;
 
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        //fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(float health)
     {
         slider.value = health;
+    }
+
+    public void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        health = player.GetComponentInParent<Health>();
+        SetMaxHealth(health.GetMaxHealth());
+    }
+
+    public void Update()
+    {
+        SetHealth(health.GetCurrentHealth());
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Play a generic Scene...");
+            health.TakeDamage(10);
+        }
+
+        //fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
